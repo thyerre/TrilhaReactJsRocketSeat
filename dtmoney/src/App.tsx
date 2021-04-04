bf672fff02d8a58053588504c5479c981de78711
Thyerre Rangel <thyerre321@gmail.com>
2021-04-04 14:20:50 -0300

    criando header

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Header/>
+      <GlobalStyle/>
+    </>
   );
 }
---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --red: #E52e4d;
+        --blue: #5429cc;
+        
+        --blue-light: #6933ff;
+        
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        
+        --background: #f0f2f5;
+        --shape: #fff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%; //15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; //14px
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased; 
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 400;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed; 
+    }
+`;
